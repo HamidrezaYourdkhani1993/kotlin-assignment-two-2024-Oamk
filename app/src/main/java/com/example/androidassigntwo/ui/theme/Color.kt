25b3f9842311d3e8811d9e13fd96cef8db8cabf0
Hamidreza <hamidreza.yourdkhani1993@gmail.com>
2024-01-31 15:53:36 +0200

    Body miss func

---
+++ app/src/main/java/com/example/androidassigntwo/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.androidassigntwo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFFF5722)
+val PurpleGrey80 = Color(0xFFFFEB3B)
+val Pink80 = Color(0xFF9C27B0)
+
+val Purple40 = Color(0xFF4300FF)
+val PurpleGrey40 = Color(0xFFAA83FF)
+val Pink40 = Color(0xFFFF0053)
